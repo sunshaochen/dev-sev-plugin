466aa0190ecf30252075533d7ce40425b25eb8f4
GitHub Action <action@github.com>
2021-09-07 07:09:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/sunshaochen/devsevplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sunshaochen/devsevplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sunshaochen.devsevplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sunshaochen.devsevplugin.MyBundle
 
 class MyApplicationService {
 
